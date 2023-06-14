b23090aef1f1def2bafb1eb2bbdac8a81150304c
shmawilton <246wiltononyango@gmail.com>
2023-06-14 23:03:32 +0300

    second

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,11 @@
 import PropTypes from 'prop-types'
 import Buttons from './Buttons'
 
-const Header = ({ title }) => {
-    const onClick = () => {
-        console.log('click');
-    }
-
+const Header = ({ title, onAdd, showAdd }) => {
   return (
     <header className='header'>
       <h1>{title}</h1>
-      <Buttons color='green' text='Add' onClick={onClick} />
+      <Buttons color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd} />
     </header>
   )
 }
